1a461814c2f8ee308bef9b94d5e5fa0d715966b1
Arjun Suthar <arjunsuthar3427@gmail.com>
2024-05-06 13:53:42 +0530

    context

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <img src="./logo.png" className="App-logo" alt="logo" />
+      <h1>Food App</h1>
+      <input type="text" placeholder="Search" />
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/context/FoodContext.js
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+import FoodData from "../FoodData";
+
+const FoodContext = createContext(FoodData);
+
+export default FoodContext;
