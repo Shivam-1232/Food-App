78042a78a20349f2bfad13d621b9a9155b68aa4b
Shivam-1232 <156029005+Shivam-1232@users.noreply.github.com>
2024-05-07 12:53:01 +0530

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { FoodContext } from "../context/foodContext"; 
 
 const Navbar = () => {
+
+  const {displayFood, setDisplayFood,handleSearch,input}=useContext(FoodContext)
   return (
     <div className="navbar">
       <img src="./ficon.svg" className="App-logo" alt="logo" />
       <h1>Food App</h1>
-      <input type="text" placeholder="Search" />
+      <input type="text" placeholder="Search" 
+      value={input}
+      onChange={handleSearch}
+      />
     </div>
   );
 };
