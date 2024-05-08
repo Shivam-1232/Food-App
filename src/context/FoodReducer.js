a6e04d364b2dd1e70312155b4a39ed45b3856f58
Shivam-1232 <156029005+Shivam-1232@users.noreply.github.com>
2024-05-08 15:12:09 +0530

    updates

--- src/context/FoodReducer.js
+++ src/context/FoodReducer.js
@@ -3,11 +3,18 @@ const FoodReducer = (state,action) => {
     const initialCategories = action.initialCategories;
 
     switch (action.type) {
-       case 'All': return {...initialCategories, All: true};
+       case 'All': 
+            return {...initialCategories, All: true};
        case 'Lunch': return {...initialCategories, Lunch: true};
-       case 'All': return {...initialCategories, All: true};
-       case 'All': return {...initialCategories, All: true};
-       case 'All': return {...initialCategories, All: true};
+            return {... initialCategories, Lunch: true};
+       case 'Breakfast': return {...initialCategories, Breakfast: true};
+            return {...initialCategories, Breakfast: true}; 
+       case 'Dinner': return {...initialCategories, Dinner: true};
+            return {...initialCategories,Dinner: true};
+       case 'Snacks': return {...initialCategories, Snacks: true};
+             return {...initialCategories, Snacks: true};
+       case 'Drinks': return {...initialCategories, Drinks: true};
+             return {...initialCategories, Drinks: true};
         default:
             return state;
     }
