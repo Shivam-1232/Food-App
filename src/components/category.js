153444d5845ba850714eccf49eb1c8c49e998f19
Shivam-1232 <156029005+Shivam-1232@users.noreply.github.com>
2024-06-12 11:49:25 +0530

    Updates

--- src/components/category.js
+++ src/components/category.js
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react'
-import { FoodContext } from '../context/FoodContext';
+import React, { useContext } from "react";
+import { FoodContext } from "../context/FoodContext";
 
 // const Category = () => {
 //   return (
@@ -19,20 +19,28 @@ import { FoodContext } from '../context/FoodContext';
 
 // export default Category
 const Category = () => {
-
-  const {category, handleClick} = useContext(FoodContext);
+  const { category, handleClick } = useContext(FoodContext);
 
   const categoryArr = Object.keys(category);
-  
+
   return (
     <div className="category">
       <h2>Find your favorite food</h2>
       <div className="category-buttons">
-        {
-          categoryArr.map((data)=>{
-              return <button key={data} style={{backgroundColor: category[data] ? '#34d399' : '#e0cccc', color: category[data] ? 'white' : 'black'}} onClick={()=>handleClick(data)} >{data}</button>
-          })
-        }
+        {categoryArr.map((data) => {
+          return (
+            <button
+              key={data}
+              style={{
+                backgroundColor: category[data] ? "#34d399" : "#e0cccc",
+                color: category[data] ? "white" : "black",
+              }}
+              onClick={() => handleClick(data)}
+            >
+              {data}
+            </button>
+          );
+        })}
       </div>
     </div>
   );
